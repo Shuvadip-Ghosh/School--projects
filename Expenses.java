6994511eb1a4bac7ea81e3d128cafec2bd1c8b55
Shuvadip Ghosh <shuvadipghosh.pkt@gmail.com>
2021-08-14 21:59:50 +0530

    Put The name of the class corectly

---
+++ Expenses.java
@@ -0,0 +1,13 @@
+// A man spends 45% of his monthly salary on food(sFood), 10% on daily transportation(sTra), 8% on clothes(sStyle), 5% on outings(sOut), 2% on charity(sChr), 10% on investments(sInv) and the rest he saves(Save). He spends 10% on holiday trips(sTrip) annually.
+// Class name: Expenses
+// Data Members/Instance variables: double Msal(monthly salary), sFood, sTra, sStyle, sOut, sChr, sInv, Save, sTrip, aExp(annual expense)
+// Member methods:
+// Expenses(): A default constructor to initialize the data members.
+// void input(): Inputs the monthly salary
+// void expenditure(): calculates the annual expenses and annual savings
+// void display(): displays the amount spent annually on food, clothing, transportation,  outings, charity, investments, annual holiday trip, annual expense and annual savings.
+// Write a main method to create a class and call the above methods.
+
+public class Expenses {
+    
+}
---
+++ Telephone_Bill.java
@@ -0,0 +1,34 @@
+// Class name: Telephone_Bill
+// Data members/Instance variables: String name(name of the consumer);
+// int bno (bill number), calls (no. of calls in a month); double billamt (bill amount to be paid)
+// Member methods:
+// Telephone_Bill(): A default constructor to initialize the data members with default values.
+// Telephone_Bill(): A parameterised constructor to accept the name, bill number and calls .
+// void calculate(): To calculate the telephone bill as per the tariff given:
+
+// Number of calls
+// Rate 
+// First 120 calls
+// Rs.100
+// Next 100 calls
+// Rs.0.95/call
+// Next 100 calls
+// Rs. 1.05/call
+// Above 320 calls
+// Rs. 1.25/call
+
+ 
+// Fixed monthly rental applicable to all consumers is Rs.195
+
+// void display(): To display the name of the consumer, bill number, no. of calls made and the bill amount to be paid in the following format:
+
+// Bill No.		Name       	    No. of calls		Bill Amount
+// XXX			XXXX		XXX			XXXX.XX
+
+// Write a main method to create an object and invoke the methods to obtain the desired output.
+
+public class Telephone_Bill {
+    public static void main(String[] args) {
+        
+    }
+}
