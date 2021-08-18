2c5315fdf9aeca68c0bd37d88413552ffc74d72e
Shuvadip-Ghosh <shuvadipghosh.pkt@gmail.com>
2021-08-18 13:27:04 +0530

    taken monthly salary input

--- Expenses.java
+++ Expenses.java
@@ -7,8 +7,26 @@
 // void expenditure(): calculates the annual expenses and annual savings
 // void display(): displays the amount spent annually on food, clothing, transportation,  outings, charity, investments, annual holiday trip, annual expense and annual savings.
 // Write a main method to create a class and call the above methods.
-
+import java.util.Scanner;
 public class Expenses {
+    double Msal, sFood, sTra, sStyle, sOut, sChr, sInv, Save, sTrip, aExp;
+    Expenses(){
+        Msal = 0.0d;
+        sFood = 0.0d;
+        sTra =0.0d;
+        sStyle = 0.0d;
+        sOut = 0.0d;
+        sChr = 0.0d;
+        sInv = 0.0d;
+        Save = 0.0d;
+        sTrip = 0.0d;
+        aExp = 0.0d;
+    }
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your monthly salary: ");
+        Msal = sc.nextDouble();
+    }
     public static void main(String[] args) {
         
     }
