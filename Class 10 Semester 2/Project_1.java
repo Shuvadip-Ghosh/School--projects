1847a56147aa274e646e005bcab8a937866c143e
Shuvadip ghosh <shuvadipdeveloper@gmail.com>
2021-12-23 12:20:24 +0530

    Made the first five files for project

---
+++ Class 10 Semester 2/Project_1.java
@@ -0,0 +1,5 @@
+public class Project_1 {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
---
+++ Class 10 Semester 2/Project_2.java
@@ -0,0 +1,5 @@
+public class Project_2 {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
