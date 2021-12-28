570473228d0bb8263e494b19bdbc6f4e82cd5e3c
Shuvadip ghosh <shuvadipdeveloper@gmail.com>
2021-12-28 14:54:29 +0530

    done program 8

--- Class 10 Semester 2/Project_8.java
+++ Class 10 Semester 2/Project_8.java
@@ -1,8 +1,31 @@
 /**
+ * Wap to input two strings and check if they are anagram (Two Strings are called the anagram if they contain the same characters. However, the order or sequence of the characters can be different.). 
+ * Example Brag and Grab
  */
+import java.util.Arrays;
+import java.util.Scanner;
 public class Project_8 {
-
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two Strings: ");
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if (str1.length() != str2.length()) {
+            System.out.println("Both the strings are not anagram");
+        } 
+        else {
+            char[] string1 = str1.toCharArray();
+            char[] string2 = str2.toCharArray();
+            Arrays.sort(string1);
+            Arrays.sort(string2);
+            if (Arrays.equals(string1, string2) == true) {
+                System.out.println("Both the strings are anagram");
+            } 
+            else {
+                System.out.println("Both the strings are not anagram");
+            }
+        }
     }
 }
