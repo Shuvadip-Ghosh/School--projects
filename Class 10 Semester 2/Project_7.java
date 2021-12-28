3be83562d7f06041b9962dbac8a375537af70069
Shuvadip ghosh <shuvadipdeveloper@gmail.com>
2021-12-28 14:41:20 +0530

    done program 7

--- Class 10 Semester 2/Project_7.java
+++ Class 10 Semester 2/Project_7.java
@@ -1,9 +1,21 @@
 /** 
- * 
+ * Wap to Input a Word and print a half diamond Pattern using the word
  */
-public class Project_7 {
+import java.util.Scanner;
 
+public class Project_7 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scan.nextLine();
+        int size = word.length();
+  
+        for (int i=0; i < (2*size-1); i++) {
+          int column = (i<size) ? i : (2*size-i-2);
+          for (int j=0; j<=column; j++) {
+            System.out.print(word.charAt(j));
+          }
+          System.out.println(); // new line
+        }
     }
 }
